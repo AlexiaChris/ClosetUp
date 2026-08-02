80d61ab54b4a42eb629173b3f82dad58357483e6
AlexiaChris <alexia.chris02@gmail.com>
2026-08-02 20:38:58 +0700

    added a file upload endpoint + background removal

--- src/closet/closet.controller.ts
+++ src/closet/closet.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post } from '@nestjs/common';
 import { Get, Body } from '@nestjs/common';
 import { ClosetService } from './closet.service';
 import { ClosetItemDto } from './dto/closetItemDto.dto';
+import { UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('closet')
 export class ClosetController {
@@ -19,6 +21,15 @@ export class ClosetController {
     @Post()
     postService(@Body() data: ClosetItemDto) {
         return this.closetService.create(data);
+    }
+
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file'))
+    uploadItem(
+        @UploadedFile() file: Express.Multer.File,
+        @Body('removeBg') removeBg: string,
+    ) {
+        return this.closetService.processUpload(file, removeBg === 'true');
     }
 
 }
--- src/closet/closet.service.ts
+++ src/closet/closet.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ClosetItemDto } from './dto/closetItemDto.dto';
+import { removeBackground } from '@imgly/background-removal-node';
+import * as fs from 'fs';
 
 @Injectable()
 export class ClosetService {
@@ -12,5 +14,23 @@ export class ClosetService {
 
     create(data: ClosetItemDto) {
         return this.prisma.closetItem.create({data});
+    }
+
+    async processUpload(file: Express.Multer.File, shouldRemoveBg: boolean) {
+        let finalBuffer: Buffer = file.buffer;
+
+        if (shouldRemoveBg) {
+            const blob = new Blob([new Uint8Array(file.buffer)], { type: file.mimetype });
+            const finalBlob = await removeBackground(blob);
+            const arrayBuffer = await finalBlob.arrayBuffer();
+            finalBuffer = Buffer.from(arrayBuffer);
+        }
+
+        fs.writeFileSync('test-output-finalBuffer.png', finalBuffer);
+
+        return { 
+            message: 'Processed', 
+            bgRemoved: shouldRemoveBg 
+        };
     }
 }
